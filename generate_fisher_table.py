37cd697b1eb5bca3239c36139a60871874a2a88f
Justin Pierce <jupierce@redhat.com>
2023-03-13 15:22:32 -0400

    Initial

---
+++ generate_fisher_table.py
@@ -0,0 +1,70 @@
+# Using fast-fisher: https://github.com/MrTomRod/fast-fisher
+import bitstring
+import math
+import os
+import fast_fisher.fast_fisher_cython
+import scipy
+from multiprocessing import Pool
+
+import time
+
+# Set to true to sanity check the generated table against scipy calculated values
+scipy_sanity_check = False
+
+MAX_CELL_SAMPLES = 512   # must be a power of 2 and divisible by 8
+# MAX_CELL_SAMPLES = 32
+# MAX_CELL_SAMPLES = 8
+MULTIPLICATION_SHIFTS = int(math.log2(MAX_CELL_SAMPLES))
+
+if (1 << MULTIPLICATION_SHIFTS) != MAX_CELL_SAMPLES or MAX_CELL_SAMPLES//8*8 != MAX_CELL_SAMPLES:
+    print(f'MAX_CELL_SAMPLES must be a power of 2 and divisible by 8')
+    exit(1)
+
+ALPHA = 0.05
+
+
+def offset(a, b, c, d) -> int:
+    return (a << (MULTIPLICATION_SHIFTS * 3)) + (b << (MULTIPLICATION_SHIFTS * 2)) + (c << (MULTIPLICATION_SHIFTS * 1)) + d
+
+
+def fisher_for_a(a: int):
+    result = bitstring.BitArray(length=MAX_CELL_SAMPLES * MAX_CELL_SAMPLES * MAX_CELL_SAMPLES)
+    for b in range(0, MAX_CELL_SAMPLES):
+        for c in range(0, MAX_CELL_SAMPLES):
+            for d in range(0, MAX_CELL_SAMPLES):
+                pvalue = fast_fisher.fast_fisher_cython.fisher_exact(a, b, c, d, alternative='greater')
+                if pvalue <= ALPHA:
+                    result.set(True, offset(0, b, c, d))
+    return a, result
+
+
+if __name__ == '__main__':
+    start = time.time_ns()
+
+    if MAX_CELL_SAMPLES > 64 and scipy_sanity_check:
+        print('It will take too long to sanity check computed tables with > 64 samples')
+        exit(1)
+
+    with open('table.bin', 'w+b') as f:
+        with Pool(max(4, os.cpu_count() - 2)) as p:
+            for result in p.imap_unordered(fisher_for_a, range(0, MAX_CELL_SAMPLES)):
+                a, bits = result
+                f.seek(a * (bits.length // 8))
+                bits.tofile(f)
+                print(result)
+
+    if scipy_sanity_check:
+        with open('table.bin', 'rb') as f:
+            all_bits = bitstring.BitArray(f)
+            for a in range(0, MAX_CELL_SAMPLES):
+                print(f'Sanity checking: {a} of {MAX_CELL_SAMPLES-1}')
+                for b in range(0, MAX_CELL_SAMPLES):
+                    for c in range(0, MAX_CELL_SAMPLES):
+                        for d in range(0, MAX_CELL_SAMPLES):
+                            _, pvalue = scipy.stats.fisher_exact([[a, b], [c, d]], alternative='greater')
+                            significant = (pvalue <= ALPHA)
+                            if significant != all_bits[offset(a, b, c, d)]:
+                                fisher_fast_pvalue = fast_fisher.fast_fisher_cython.fisher_exact(a, b, c, d, alternative="greater")
+                                if abs(fisher_fast_pvalue - pvalue) > 0.0000001:
+                                    print(f'Difference at a={a} b={b} c={c} d={d} ; scipy got={pvalue}    and   fast got={fisher_fast_pvalue}')
+
---
+++ readiness/main/views.py
@@ -0,0 +1,212 @@
+import re
+import math
+import mmap
+from typing import Dict, NamedTuple, List
+from enum import Enum
+from collections import OrderedDict
+
+from django.shortcuts import render
+from readiness.settings import BASE_DIR
+
+from google.cloud import bigquery
+
+TestId = str
+ComponentName = str
+CapabilityName = str
+
+MAX_CELL_SAMPLES = 512   # must be a power of 2 and divisible by 8
+MULTIPLICATION_SHIFTS = int(math.log2(MAX_CELL_SAMPLES))
+
+if (1 << MULTIPLICATION_SHIFTS) != MAX_CELL_SAMPLES or MAX_CELL_SAMPLES//8*8 != MAX_CELL_SAMPLES:
+    print(f'MAX_CELL_SAMPLES must be a power of 2 and divisible by 8')
+    exit(1)
+
+ALPHA = 0.05
+
+table_bin = open(f'{BASE_DIR}/../table.bin', 'rb')
+table_mmap = mmap.mmap(table_bin.fileno(), length=0, access=mmap.ACCESS_READ)
+
+
+def fisher_offset(a, b, c, d) -> int:
+    return (a << (MULTIPLICATION_SHIFTS * 3)) + (b << (MULTIPLICATION_SHIFTS * 2)) + (c << (MULTIPLICATION_SHIFTS * 1)) + d
+
+
+def fisher_significant(a, b, c, d) -> bool:
+    offset = fisher_offset(a, b, c, d)
+    bin_offset = offset // 8
+    return table_mmap[bin_offset] & (1 << (offset % 8)) > 0
+
+
+class TestRecord(NamedTuple):
+    test_id: TestId
+    test_name: str
+    success_count: int
+    failure_count: int
+    total_count: int
+
+
+class Conclusion(Enum):
+    MISSING_IN_BASIS = 1
+    SIGNIFICANT_IMPROVEMENT = 2
+    SIGNIFICANT_REGRESSION = 3
+
+
+ConclusionMap = Dict[TestId, Conclusion]
+
+
+def has_regression(records: List[TestRecord], conclusions: ConclusionMap) -> bool:
+    """
+    :param records: The records to analyze
+    :param conclusions: The conclusions for all tests analyzed
+    :return: Returns True if even on TestRecord is a regression.
+    """
+    for record in records:
+        conclusion = conclusions.get(record.test_id, None)
+        if conclusion:
+            if conclusion.SIGNIFICANT_REGRESSION:
+                return True
+    return False
+
+
+class CapabilityRecords(NamedTuple):
+    name: CapabilityName
+    test_records: Dict[TestId, TestRecord]
+
+    def has_regressed(self) -> bool:
+        return has_regression(self.test_records.values())
+
+    def register_test_record(self, test_record: TestRecord):
+        self.test_records[test_record.test_id] = test_record
+
+
+ByCapability = Dict[CapabilityName, CapabilityRecords]
+
+
+class ComponentRecords(NamedTuple):
+    name: ComponentName
+    test_records: Dict[TestId, TestRecord]
+    capabilities: ByCapability
+
+    def has_regressed(self, conclusions) -> bool:
+        return has_regression(self.test_records.values(), conclusions=conclusions)
+
+    def register_test_record(self, test_record: TestRecord):
+        self.test_records[test_record.test_id] = test_record
+
+    def register_test_record_capability(self, capability_name: CapabilityName, test_record: TestRecord):
+        if capability_name not in self.capabilities:
+            self.capabilities[capability_name] = CapabilityRecords(capability_name, list())
+        self.capabilities[capability_name].test_records.append(test_record)
+
+
+ById = Dict[TestId, TestRecord]
+ByComponent = Dict[ComponentName, ComponentRecords]
+
+
+def index(request):
+    return render(request, "main/index.html")
+
+
+component_pattern = re.compile(r'\[[^\\]+?\]')
+
+
+def categorize(rows) -> (ById, ByComponent):
+    by_id: ById = dict()
+    by_component: ByComponent = dict()
+
+    for row in rows:
+        if row['success_count'] is None:
+            # Not sure why, but bigquery can return None for some SUMs.
+            # Potentially because success_val was not set on all original rows
+            continue
+        r = TestRecord(
+            test_id=row['test_id'],
+            test_name=row['test_name'],
+            success_count=row['success_count'],
+            failure_count=row['total_count'] - row['success_count'],
+            total_count=row['total_count'],
+        )
+        by_id[r.test_id] = r
+        labels = re.findall(component_pattern, r.test_name)
+        for label in labels:
+            if label not in by_component:
+                by_component[label] = ComponentRecords(name=label, test_records=dict(), capabilities=dict())
+            by_component[label].register_test_record(r)
+
+            capability = r.test_id[:3]
+            by_component[label].register_test_record_capability(capability, r)
+
+    return by_id, by_component
+
+
+def calculate_conclusions(basis_by_id: ById, samples_by_id: ById) -> ConclusionMap:
+    by_conclusion: ConclusionMap = dict()
+    for test_id, sample_result in samples_by_id.items():
+        basis_result = basis_by_id.get(test_id, None)
+        if not basis_result:
+            by_conclusion[test_id] = Conclusion.MISSING_IN_BASIS
+            continue
+
+        basis_pass_percentage = basis_result.success_count / basis_result.total_count
+        sample_pass_percentage = sample_result.success_count / sample_result.total_count
+        improved = sample_pass_percentage >= basis_pass_percentage
+
+        significant = fisher_significant(
+            sample_result.failure_count,
+            sample_result.success_count,
+            basis_result.failure_count,
+            basis_result.success_count,
+        )
+
+        if significant:
+            by_conclusion[test_id] = Conclusion.SIGNIFICANT_IMPROVEMENT if improved else Conclusion.SIGNIFICANT_REGRESSION
+
+    return by_conclusion
+
+
+def report(request):
+    basis_release = request.GET['basis_release']
+    basis_start_dt = request.GET['basis_start_dt']
+    basis_end_dt = request.GET['basis_end_dt']
+
+    sample_release = request.GET['sample_release']
+    sample_start_dt = request.GET['sample_start_dt']
+    sample_end_dt = request.GET['sample_end_dt']
+
+    bq = bigquery.Client()
+    q = f'''
+        SELECT test_id, ANY_VALUE(test_name) as test_name, COUNT(*) as total_count, SUM(success_val) as success_count 
+        FROM `openshift-gce-devel.ci_analysis_us.junit` 
+        WHERE modified_time >= DATETIME(TIMESTAMP "{basis_start_dt}:00+00") AND modified_time < DATETIME(TIMESTAMP "{basis_end_dt}:00+00") 
+              AND branch = "{basis_release}" AND file_path NOT LIKE "%/junit_operator.xml" GROUP BY test_id        
+    '''
+
+    basis_rows = bq.query(q)
+    basis_by_id, basis_by_component = categorize(basis_rows)
+
+    q = f'''
+        SELECT test_id, ANY_VALUE(test_name) as test_name, COUNT(*) as total_count, SUM(success_val) as success_count
+        FROM `openshift-gce-devel.ci_analysis_us.junit` 
+        WHERE modified_time >= DATETIME(TIMESTAMP "{sample_start_dt}:00+00") AND modified_time < DATETIME(TIMESTAMP "{sample_end_dt}:00+00") 
+              AND branch = "{sample_release}" AND file_path NOT LIKE "%/junit_operator.xml" GROUP BY test_id        
+    '''
+
+    sample_rows = bq.query(q)
+    samples_by_id, samples_by_component = categorize(sample_rows)
+
+    conclusions_by_id = calculate_conclusions(basis_by_id=basis_by_id, samples_by_id=samples_by_id)
+    component_summary: Dict[ComponentName, bool] = dict()
+    for component_name in sorted(list(samples_by_component.keys())):
+        component_summary[component_name] = samples_by_component[component_name].has_regressed(conclusions_by_id)
+
+    context = {
+        'basis_release': basis_release,
+        'basis_start_dt': basis_start_dt,
+        'basis_end_dt': basis_end_dt,
+        'sample_release': sample_release,
+        'sample_start_dt': sample_start_dt,
+        'sample_end_dt': sample_end_dt,
+        'summary': component_summary,
+    }
+
+    return render(request, 'main/report-component.html', context)
